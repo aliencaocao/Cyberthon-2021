6dadc299434f2717abc7c40b1991a7d05e7dfcea
Billy Cao <aliencaocao@gmail.com>
2021-04-03 17:17:00 +0800

    Initial Commit

---
+++ Reverse RSA.py
@@ -0,0 +1,17 @@
+from binascii import unhexlify
+
+c = input('c: ')
+p = input('p: leave empty if dont have')
+q = input('q: leave empty if dont have')
+d = input('d: ')
+n = input('n: leave empty if dont have')
+if p == '' and q == '':
+    if n == '':
+        print('n is required if either p or q is unknown!')
+elif n == '':
+    if p == '' and q == '':
+        print('p and q are required if either p or q is unknown!')
+    else:
+        n = p * q
+m = pow(c, d, n)
+print(unhexlify(hex(m)[2:]).decode())
